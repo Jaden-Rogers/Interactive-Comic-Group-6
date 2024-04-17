416658ec62e9a1d315c32c8fccb989950740c66a
Jakob A Sikes <jakobsikes@u.boisestate.edu>
2024-04-16 20:58:13 -0600

    Act2Minigame
    Added Fog of war, made progress on sound effects.

--- GIMM 250 Project - Sim or Random/Assets/Scenes/Jakob Scenes/TestScenes/TopDownMovement.cs
+++ GIMM 250 Project - Sim or Random/Assets/Scenes/Jakob Scenes/TestScenes/TopDownMovement.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class TopDownMovement : MonoBehaviour
 {
-
     public float speed; // Speed given to attached object
     public Rigidbody2D rb; // Reference to the Rigidbody component, attached to the GameObject
     private Vector2 moveInput; // Vector to store input for movement
- 
+
+    [SerializeField] private AudioClip ratSound;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void Update()
     {
         // Get the horizontal and vertical input from the player, left right up down keys
@@ -21,5 +28,46 @@ public class TopDownMovement : MonoBehaviour
         // Set the velocity of the Rigidbody to move the object in the desired direction
         // Multiplied by the speed to control actual movement speed of attached object
         rb.velocity = moveInput * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if collided with an object tagged as "Icon"
+        if (other.CompareTag("TP"))
+        {
+            // Play the sound effect associated with the icon
+            if (ratSound != null && audioSource != null)
+            {
+                audioSource.clip = ratSound;
+                audioSource.Play();
+            }
+        }
+        if (other.CompareTag("Rat"))
+        {
+            // Play the sound effect associated with the icon
+            if (ratSound != null && audioSource != null)
+            {
+                audioSource.clip = ratSound;
+                audioSource.Play();
+            }
+        }
+        if (other.CompareTag("Handcuffs"))
+        {
+            // Play the sound effect associated with the icon
+            if (ratSound != null && audioSource != null)
+            {
+                audioSource.clip = ratSound;
+                audioSource.Play();
+            }
+        }
+        if (other.CompareTag("Toothbrush"))
+        {
+            // Play the sound effect associated with the icon
+            if (ratSound != null && audioSource != null)
+            {
+                audioSource.clip = ratSound;
+                audioSource.Play();
+            }
+        }
     }
 }
