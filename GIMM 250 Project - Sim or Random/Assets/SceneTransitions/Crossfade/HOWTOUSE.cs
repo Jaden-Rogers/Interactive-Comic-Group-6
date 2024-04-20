b930e6fcc606000555c9488af1899cd95229240e
Jaden-Rogers <141758224+Jaden-Rogers@users.noreply.github.com>
2024-04-20 13:36:34 -0600

    Added Main Menu

--- GIMM 250 Project - Sim or Random/Assets/SceneTransitions/Crossfade/HOWTOUSE.cs
+++ GIMM 250 Project - Sim or Random/Assets/SceneTransitions/Crossfade/HOWTOUSE.cs
@@ -1,2 +1,4 @@
-// In order to use crossfade, add the prefab to a canvas in the scene
-// Call the LevelLoader script methods from a button or other event to trigger the transition
+// To use crossfade, add the prefab to a canvas in the scene
+// the transition will play when the scene is loaded
+// Call the LevelLoader script methods from a button or other event to trigger the fade to black transition
+// Alternatively, create a method that uses transition.SetTrigger("Start"); to trigger the fade to black transition
